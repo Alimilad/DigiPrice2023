d8017cd6d0843028ba4375dca9e847e35962886b
dotnethelp <65740469+dotnethelp@users.noreply.github.com>
2023-10-04 17:42:27 +0330

    netlify

--- DigiPrice/netlify.build.sh
+++ DigiPrice/netlify.build.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
+#!/usr/bin/env bash
+set -e
+
+## install latest .NET 7.0 release
+pushd /tmp
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+chmod u+x /tmp/dotnet-install.sh
+/tmp/dotnet-install.sh --channel 7.0
+popd
+
+## publish project to known location for subsequent deployment by Netlify
+dotnet publish -c Release -o release
